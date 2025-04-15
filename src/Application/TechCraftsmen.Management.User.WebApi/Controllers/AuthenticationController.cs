42e44145f185eef20c6d457c464137d4e898ef05
Artur Rios <arturdev@duck.com>
2025-04-15 20:55:18 -0300

    feat: add authentication endpoint

---
+++ src/Application/TechCraftsmen.Management.User.WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechCraftsmen.Core.WebApi;
+using TechCraftsmen.Core.WebApi.Security.Attributes;
+using TechCraftsmen.Core.WebApi.Security.Records;
+using TechCraftsmen.Management.User.Services;
+
+namespace TechCraftsmen.Management.User.WebApi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class AuthenticationController(AuthenticationService authenticationService) : BaseController
+{
+    [HttpPost]
+    [Route("User")]
+    [AllowAnonymous]
+    public ActionResult<WebApiOutput<Authentication>> AuthenticateUser([FromBody] Credentials credentials)
+    {
+        return Resolve(authenticationService.AuthenticateUser(credentials)!);
+    }
+}
