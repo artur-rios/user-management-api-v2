48a5d16c31f2f338effea96ae0a5700d4a901a32
Artur Rios <arturdev@duck.com>
2025-04-15 12:06:27 -0300

    feat: parametrize token configuration

--- src/Core/TechCraftsmen.Management.User.Services/AuthenticationService.cs
+++ src/Core/TechCraftsmen.Management.User.Services/AuthenticationService.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.Extensions.Options;
 using TechCraftsmen.Core.Data;
 using TechCraftsmen.Core.Extensions;
 using TechCraftsmen.Core.Output;
@@ -6,6 +7,7 @@ using TechCraftsmen.Core.Validation;
 using TechCraftsmen.Core.WebApi.Security.Interfaces;
 using TechCraftsmen.Core.WebApi.Security.Records;
 using TechCraftsmen.Management.User.Domain.Filters;
+using TechCraftsmen.Management.User.Services.Exceptions;
 
 namespace TechCraftsmen.Management.User.Services;
 
@@ -13,11 +15,17 @@ public class AuthenticationService : IAuthenticationService
 {
     private readonly IValidator<Credentials> _credentialsValidator;
     private readonly ICrudRepository<Domain.Aggregates.User> _userRepository;
+    private readonly JwtTokenConfiguration _jwtTokenConfiguration;
+    private readonly IValidator<JwtTokenConfiguration> _jwtTokenConfigurationValidator;
 
-    public AuthenticationService(IValidator<Credentials> credentialsValidator, ICrudRepository<Domain.Aggregates.User> userRepository)
+    public AuthenticationService(IValidator<Credentials> credentialsValidator, ICrudRepository<Domain.Aggregates.User> userRepository, IOptions<JwtTokenConfiguration> jwtTokenConfiguration, IValidator<JwtTokenConfiguration> jwtTokenConfigurationValidator)
     {
-        _userRepository = userRepository;
         _credentialsValidator = credentialsValidator;
+        _userRepository = userRepository;
+        _jwtTokenConfiguration = jwtTokenConfiguration.Value;
+        _jwtTokenConfigurationValidator = jwtTokenConfigurationValidator;
+        
+        ValidateTokenConfigAndThrow();
     }
     
     public DataOutput<Authentication> AuthenticateUser(Credentials credentials)
@@ -34,18 +42,17 @@ public class AuthenticationService : IAuthenticationService
         
         if (user == null)
         {
-            return new DataOutput<Authentication>(default, ["Invalid credentials"], false);
+            return new DataOutput<Authentication>(null, ["Invalid credentials"], false);
         }
         
         var passwordHash = new Hash(user.Password, user.Salt);
         
         if (!passwordHash.TextMatches(credentials.Password))
         {
-            return new DataOutput<Authentication>(default, ["Invalid credentials"], false);
+            return new DataOutput<Authentication>(null, ["Invalid credentials"], false);
         }
         
-        // TODO: parametrize token configuration
-        var jwtToken = new JwtToken(user.Id, new JwtTokenConfiguration(86400, "issuer", "audience", "secret"));
+        var jwtToken = new JwtToken(user.Id, _jwtTokenConfiguration);
 
         var authentication = new Authentication(jwtToken.Token, true, jwtToken.CreatedAt, jwtToken.Expiration);
         
@@ -54,13 +61,12 @@ public class AuthenticationService : IAuthenticationService
 
     public DataOutput<AuthenticatedUser> ValidateTokenAndGetUser(string token)
     {
-        // TODO: parametrize secret
-        var jwtToken = new JwtToken(token, "secret");
+        var jwtToken = new JwtToken(token, _jwtTokenConfiguration.Secret);
         var isValid = jwtToken.IsTokenValid().GetAwaiter().GetResult();
         
         if (!isValid)
         {
-            return new DataOutput<AuthenticatedUser>(default, ["Invalid token"], false);
+            return new DataOutput<AuthenticatedUser>(null, ["Invalid token"], false);
         }
         
         var userId = jwtToken.GetUserId();
@@ -69,9 +75,23 @@ public class AuthenticationService : IAuthenticationService
 
         if (user is null)
         {
-            return new DataOutput<AuthenticatedUser>(default, ["User not found"], false);
+            return new DataOutput<AuthenticatedUser>(null, ["User not found"], false);
         }
         
         return new DataOutput<AuthenticatedUser>(new AuthenticatedUser(user.Id, user.RoleId), ["Auth token is valid", "User retrieved"], true);
+    }
+    
+    private void ValidateTokenConfigAndThrow()
+    {
+        var validationResult = _jwtTokenConfigurationValidator.Validate(_jwtTokenConfiguration);
+
+        if (validationResult.IsValid)
+        {
+            return;
+        }
+
+        var validationErrors = validationResult.Errors.Select(vf => vf.ErrorMessage).ToArray();
+
+        throw new MissingConfigurationException(validationErrors);
     }
 }
---
+++ src/Core/TechCraftsmen.Management.User.Services/Exceptions/MissingConfigurationException.cs
@@ -0,0 +1,5 @@
+﻿using TechCraftsmen.Core.Output;
+
+namespace TechCraftsmen.Management.User.Services.Exceptions;
+
+public class MissingConfigurationException(string[] messages, string message = "Internal error") : CustomException(messages, message);
