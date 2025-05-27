a34e0ba254a3bfdee69c568807ffdd3ff6913d1c
Artur Rios <arturdev@duck.com>
2025-05-26 23:27:46 -0300

    docs: add comment about special char problem on .env files

--- src/Core/TechCraftsmen.Management.User.Services/UserService.cs
+++ src/Core/TechCraftsmen.Management.User.Services/UserService.cs
@@ -26,32 +26,32 @@ public class UserService(
         }
 
         var filter = new UserFilter { Email = userDto.Email };
-        
+
         var userSearch = userRepository.GetByFilter(filter);
-        
+
         if (userSearch.Any())
         {
             return new DataOutput<int>(0, ["E-mail already registered"], false);
         }
-        
+
         var user = userDto.ToEntity();
-        
+
         httpContextAccessor.HttpContext!.Items.TryGetValue("User", out var userData);
-        
+
         var authenticatedUser = userData as AuthenticatedUser;
-        
+
         var canRegister = user.CanRegister(authenticatedUser!.Role);
 
         if (!canRegister.Success)
         {
             return new DataOutput<int>(0, canRegister.Errors.ToArray(), false);
         }
-        
+
         var hash = Hash.NewFromText(userDto.Password);
-        
+
         user.Password = hash.Value;
         user.Salt = hash.Salt;
-        
+
         var userId = userRepository.Create(user);
 
         return new DataOutput<int>(userId, ["User created with success"], true);
@@ -60,46 +60,46 @@ public class UserService(
     public DataOutput<UserDto> GetUserById(int id)
     {
         var user = userRepository.GetById(id);
-        
+
         var message = user is null ? "User not found" : "User found";
-        
+
         return new DataOutput<UserDto>(user?.ToDto(), [message], user is not null);
     }
-    
+
     public DataOutput<IList<UserDto>> GetUsersByFilter(UserFilter filter)
     {
         var userSearch = userRepository.GetByFilter(filter).ToList();
-        
+
         if (userSearch.Count == 0)
         {
             return new DataOutput<IList<UserDto>>([], ["No users found for the given filter"], false);
         }
-        
+
         var users = userSearch.Select(user => user.ToDto()).ToList();
-        
+
         return new DataOutput<IList<UserDto>>(users, ["Users found"], true);
     }
-    
+
     public DataOutput<UserDto?> UpdateUser(UserDto userDto)
     {
         var currentUser = userRepository.GetById(userDto.Id);
-        
+
         if (currentUser is null)
         {
             return new DataOutput<UserDto?>(null, ["User not found"], false);
         }
-        
+
         var canUpdate = currentUser.CanUpdate();
 
         if (!canUpdate.Success)
         {
             return new DataOutput<UserDto?>(null, canUpdate.Errors.ToArray(), false);
         }
-        
+
         var user = userDto.ToEntity();
 
         MergeUser(currentUser, user);
-        
+
         userRepository.Update(user);
 
         return new DataOutput<UserDto?>(user.ToDto(), ["User updated with success"], true);
@@ -108,68 +108,68 @@ public class UserService(
     public ProcessOutput ActivateUser(int id)
     {
         var user = userRepository.GetById(id);
-        
+
         if (user is null)
         {
             return new ProcessOutput(["User not found"]);
         }
-        
+
         var canActivate = user.CanActivate();
-        
+
         if (!canActivate.Success)
         {
             return new ProcessOutput(canActivate.Errors.ToArray());
         }
-        
+
         user.Active = true;
         userRepository.Update(user);
-        
+
         return new ProcessOutput();
     }
-    
+
     public ProcessOutput DeactivateUser(int id)
     {
         var user = userRepository.GetById(id);
-        
+
         if (user is null)
         {
             return new ProcessOutput(["User not found"]);
         }
-        
+
         var canDeactivate = user.CanDeactivate();
-        
+
         if (!canDeactivate.Success)
         {
             return new ProcessOutput(canDeactivate.Errors.ToArray());
         }
-        
+
         user.Active = false;
         userRepository.Update(user);
-        
+
         return new ProcessOutput();
     }
-    
+
     public ProcessOutput DeleteUser(int id)
     {
         var user = userRepository.GetById(id);
-        
+
         if (user is null)
         {
             return new ProcessOutput(["User not found"]);
         }
-        
+
         var canDelete = user.CanDelete();
-        
+
         if (!canDelete.Success)
         {
             return new ProcessOutput(canDelete.Errors.ToArray());
         }
-        
+
         userRepository.Delete(id);
-        
+
         return new ProcessOutput();
     }
-    
+
     private static void MergeUser(Domain.Aggregates.User source, Domain.Aggregates.User target, bool mergeStatus = true)
     {
         target.Password = source.Password;
