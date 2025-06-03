e0a12d2deb0eb3c839e5c6810723090e2c2cc2c5
Artur Rios <arturdev@duck.com>
2025-06-03 18:31:53 -0300

    test: add user functional tests initialization

--- tests/Application/TechCraftsmen.Management.User.WebApi.Tests/UserTests.cs
+++ tests/Application/TechCraftsmen.Management.User.WebApi.Tests/UserTests.cs
@@ -1,6 +1,29 @@
-﻿namespace TechCraftsmen.Management.User.WebApi.Tests;
+﻿using TechCraftsmen.Core.Environment;
+using TechCraftsmen.Core.Test;
+using TechCraftsmen.Core.WebApi.Security.Records;
+using TechCraftsmen.Management.User.Test.Fixture;
 
-public class UserTests
+namespace TechCraftsmen.Management.User.WebApi.Tests;
+
+public class UserTests(DatabaseFixture fixture, EnvironmentType environment = EnvironmentType.Local)
+    : WebApiTest<Program>(environment), IClassFixture<DatabaseFixture>, IAsyncLifetime
 {
+    private const string AuthenticationRoute = "/Authentication";
+    private const string UserRoute = "/User";
     
+    public async Task InitializeAsync()
+    {
+        Credentials credentials = new()
+        {
+            Email = fixture.TestUser.Email, 
+            Password = fixture.TestPassword
+        };
+
+        await AuthenticateAndAuthorize(credentials, AuthenticationRoute);
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
 }
