f45a0b445cda07d8342a5af11676caaa0cb2eee5
Artur Rios <arturdev@duck.com>
2025-06-06 00:52:42 -0300

    test: add user functional test case

--- tests/Application/TechCraftsmen.Management.User.WebApi.Tests/UserTests.cs
+++ tests/Application/TechCraftsmen.Management.User.WebApi.Tests/UserTests.cs
@@ -1,6 +1,9 @@
-﻿using TechCraftsmen.Core.Environment;
+﻿using System.Net;
+using TechCraftsmen.Core.Environment;
 using TechCraftsmen.Core.Test;
+using TechCraftsmen.Core.Test.Attributes;
 using TechCraftsmen.Core.WebApi.Security.Records;
+using TechCraftsmen.Management.User.Dto;
 using TechCraftsmen.Management.User.Test.Fixture;
 
 namespace TechCraftsmen.Management.User.WebApi.Tests;
@@ -25,5 +28,24 @@ public class UserTests(DatabaseFixture fixture, EnvironmentType environment = En
     public Task DisposeAsync()
     {
         return Task.CompletedTask;
+    }
+
+    [FunctionalFact]
+    public async Task Should_GetUsersByFilter()
+    {
+        var query = $"?Email={fixture.TestUser.Email}";
+        
+        var result = await Get<IList<UserDto>>($"{UserRoute}/Filter{query}", HttpStatusCode.OK);
+        
+        Assert.NotNull(result);
+        Assert.True(result.Success);
+        Assert.Equal("Search completed with success", result.Messages.First());
+        Assert.NotNull(result.Data);
+        
+        var userFound = result.Data.FirstOrDefault();
+        
+        Assert.Equal(fixture.TestUser.Name, userFound?.Name);
+        Assert.Equal(fixture.TestUser.Email, userFound?.Email);
+        Assert.Equal(fixture.TestUser.RoleId, userFound?.RoleId);
     }
 }
