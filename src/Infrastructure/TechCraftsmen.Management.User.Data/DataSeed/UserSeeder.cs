37b37afd8510dcbd469d0bcec8a39895e5bc8c8c
Artur Rios <arturdev@duck.com>
2025-06-06 00:51:46 -0300

    feat: add regular and test users on seed

--- src/Infrastructure/TechCraftsmen.Management.User.Data/DataSeed/UserSeeder.cs
+++ src/Infrastructure/TechCraftsmen.Management.User.Data/DataSeed/UserSeeder.cs
@@ -42,6 +42,32 @@ public static class UserSeeder
 
         context.Users.Add(masterUser);
         
+        var regularUserName = Environment.GetEnvironmentVariable("REGULAR_USER_NAME");
+        var regularUserEmail = Environment.GetEnvironmentVariable("REGULAR_USER_EMAIL");
+        var regularUserPassword = Environment.GetEnvironmentVariable("REGULAR_USER_PASSWORD");
+        
+        var hasRegularUserData = !(string.IsNullOrWhiteSpace(regularUserName) ||
+                                string.IsNullOrWhiteSpace(regularUserEmail) ||
+                                string.IsNullOrWhiteSpace(regularUserPassword));
+        
+        if (hasRegularUserData)
+        {
+            var regularPasswordHash = Hash.NewFromText(regularUserPassword!);
+            
+            var testUser = new Domain.Aggregates.User
+            {
+                Name = regularUserName!,
+                Email = regularUserEmail!,
+                Password = regularPasswordHash.Value,
+                Salt = regularPasswordHash.Salt,
+                RoleId = (int)Roles.Regular,
+                CreatedAt = DateTime.UtcNow,
+                Active = true
+            };
+
+            context.Users.Add(testUser);
+        }
+        
         var testUserName = Environment.GetEnvironmentVariable("TEST_USER_NAME");
         var testUserEmail = Environment.GetEnvironmentVariable("TEST_USER_EMAIL");
         var testUserPassword = Environment.GetEnvironmentVariable("TEST_USER_PASSWORD");
@@ -60,7 +86,7 @@ public static class UserSeeder
                 Email = testUserEmail!,
                 Password = testPasswordHash.Value,
                 Salt = testPasswordHash.Salt,
-                RoleId = (int)Roles.Regular,
+                RoleId = (int)Roles.Test,
                 CreatedAt = DateTime.UtcNow,
                 Active = true
             };
