de53656d7e192b4ac3ef2f3d6e08b7ef358f37f4
Artur Rios <arturdev@duck.com>
2025-06-26 22:39:00 -0300

    refactor: improve user db seed

--- src/Infrastructure/TechCraftsmen.Management.User.Data/DataSeed/UserSeeder.cs
+++ src/Infrastructure/TechCraftsmen.Management.User.Data/DataSeed/UserSeeder.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TechCraftsmen.Core.Util.Hashing;
+﻿using TechCraftsmen.Core.Util.Hashing;
 using TechCraftsmen.Management.User.Data.Configuration;
 using TechCraftsmen.Management.User.Domain.Enums;
 
@@ -9,91 +8,74 @@ public static class UserSeeder
 {
     public static async Task CreateInitialUsers(RelationalDbContext context)
     {
-        if (await context.Users.AnyAsync())
+        if (!UserWithRoleExists(context, Roles.Admin))
         {
-            return;
+            var masterUserName = Environment.GetEnvironmentVariable("MASTER_USER_NAME");
+            var masterUserEmail = Environment.GetEnvironmentVariable("MASTER_USER_EMAIL");
+            var masterUserPassword = Environment.GetEnvironmentVariable("MASTER_USER_PASSWORD");
+
+            if (!HasUserData(masterUserName, masterUserEmail, masterUserPassword))
+            {
+                throw new ArgumentException("Master user configuration is incomplete, cannot create master user");
+            }
+
+            CreateUser(context, masterUserName!, masterUserEmail!, masterUserPassword!, Roles.Admin);
         }
 
-        var masterUserName = Environment.GetEnvironmentVariable("MASTER_USER_NAME");
-        var masterUserEmail = Environment.GetEnvironmentVariable("MASTER_USER_EMAIL");
-        var masterUserPassword = Environment.GetEnvironmentVariable("MASTER_USER_PASSWORD");
-
-        var hasMasterUserData = !(string.IsNullOrWhiteSpace(masterUserName) ||
-                                string.IsNullOrWhiteSpace(masterUserEmail) ||
-                                string.IsNullOrWhiteSpace(masterUserPassword));
-
-        if (!hasMasterUserData)
+        if (!UserWithRoleExists(context, Roles.Regular))
         {
-            throw new ArgumentException("Master user configuration is incomplete. Cannot create master user.");
+            var regularUserName = Environment.GetEnvironmentVariable("REGULAR_USER_NAME");
+            var regularUserEmail = Environment.GetEnvironmentVariable("REGULAR_USER_EMAIL");
+            var regularUserPassword = Environment.GetEnvironmentVariable("REGULAR_USER_PASSWORD");
+            
+            if (HasUserData(regularUserName, regularUserEmail, regularUserPassword))
+            {
+                CreateUser(context, regularUserName!, regularUserEmail!, regularUserPassword!, Roles.Regular);
+            }
         }
 
-        var masterPasswordHash = Hash.NewFromText(masterUserPassword!);
-
-        var masterUser = new Domain.Aggregates.User
+        if (!UserWithRoleExists(context, Roles.Test))
         {
-            Name = masterUserName!,
-            Email = masterUserEmail!,
-            Password = masterPasswordHash.Value,
-            Salt = masterPasswordHash.Salt,
-            RoleId = (int)Roles.Admin,
+            var testUserName = Environment.GetEnvironmentVariable("TEST_USER_NAME");
+            var testUserEmail = Environment.GetEnvironmentVariable("TEST_USER_EMAIL");
+            var testUserPassword = Environment.GetEnvironmentVariable("TEST_USER_PASSWORD");
+
+            if (HasUserData(testUserName, testUserEmail, testUserPassword))
+            {
+                CreateUser(context, testUserName!, testUserEmail!, testUserPassword!, Roles.Test);
+            }
+        }
+
+        await context.SaveChangesAsync();
+    }
+
+    private static void CreateUser(RelationalDbContext context, string name, string email, string password, Roles role)
+    {
+        var passwordHash = Hash.NewFromText(password);
+
+        var user = new Domain.Aggregates.User
+        {
+            Name = name,
+            Email = email,
+            Password = passwordHash.Value,
+            Salt = passwordHash.Salt,
+            RoleId = (int)role,
             CreatedAt = DateTime.UtcNow,
             Active = true
         };
 
-        context.Users.Add(masterUser);
-        
-        var regularUserName = Environment.GetEnvironmentVariable("REGULAR_USER_NAME");
-        var regularUserEmail = Environment.GetEnvironmentVariable("REGULAR_USER_EMAIL");
-        var regularUserPassword = Environment.GetEnvironmentVariable("REGULAR_USER_PASSWORD");
-        
-        var hasRegularUserData = !(string.IsNullOrWhiteSpace(regularUserName) ||
-                                string.IsNullOrWhiteSpace(regularUserEmail) ||
-                                string.IsNullOrWhiteSpace(regularUserPassword));
-        
-        if (hasRegularUserData)
-        {
-            var regularPasswordHash = Hash.NewFromText(regularUserPassword!);
-            
-            var testUser = new Domain.Aggregates.User
-            {
-                Name = regularUserName!,
-                Email = regularUserEmail!,
-                Password = regularPasswordHash.Value,
-                Salt = regularPasswordHash.Salt,
-                RoleId = (int)Roles.Regular,
-                CreatedAt = DateTime.UtcNow,
-                Active = true
-            };
+        context.Users.Add(user);
+    }
 
-            context.Users.Add(testUser);
-        }
-        
-        var testUserName = Environment.GetEnvironmentVariable("TEST_USER_NAME");
-        var testUserEmail = Environment.GetEnvironmentVariable("TEST_USER_EMAIL");
-        var testUserPassword = Environment.GetEnvironmentVariable("TEST_USER_PASSWORD");
-        
-        var hasTestUserData = !(string.IsNullOrWhiteSpace(testUserName) ||
-                                string.IsNullOrWhiteSpace(testUserEmail) ||
-                                string.IsNullOrWhiteSpace(testUserPassword));
-        
-        if (hasTestUserData)
-        {
-            var testPasswordHash = Hash.NewFromText(testUserPassword!);
-            
-            var testUser = new Domain.Aggregates.User
-            {
-                Name = testUserName!,
-                Email = testUserEmail!,
-                Password = testPasswordHash.Value,
-                Salt = testPasswordHash.Salt,
-                RoleId = (int)Roles.Test,
-                CreatedAt = DateTime.UtcNow,
-                Active = true
-            };
+    private static bool HasUserData(string? username, string? email, string? password)
+    {
+        return !(string.IsNullOrWhiteSpace(username) ||
+                 string.IsNullOrWhiteSpace(email) ||
+                 string.IsNullOrWhiteSpace(password));
+    }
 
-            context.Users.Add(testUser);
-        }
-        
-        await context.SaveChangesAsync();
+    private static bool UserWithRoleExists(RelationalDbContext context, Roles role)
+    {
+        return context.Users.Any(user => user.RoleId == (int)role);
     }
 }
