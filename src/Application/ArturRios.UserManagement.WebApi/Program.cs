8cd569498d5b81d9ed62005204ec248d50756c8a
Artur Rios <arturdev@duck.com>
2025-08-29 19:34:39 -0300

    feat: move web api configuration to startup class

--- src/Application/ArturRios.UserManagement.WebApi/Program.cs
+++ src/Application/ArturRios.UserManagement.WebApi/Program.cs
@@ -1,86 +1,16 @@
-﻿using ArturRios.Common.Environment;
-using ArturRios.Common.Output;
-using ArturRios.Common.WebApi;
-using ArturRios.Common.WebApi.Security.Middleware;
-using ArturRios.UserManagement.Data.Configuration;
-using ArturRios.UserManagement.WebApi.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
-
-namespace ArturRios.UserManagement.WebApi;
+﻿namespace ArturRios.UserManagement.WebApi;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        var environment = builder.Environment.EnvironmentName;
-        var basePath = AppDomain.CurrentDomain.BaseDirectory;
-        var envFolder = Path.Combine(basePath, "Environments");
-        var envFile = Path.Combine(envFolder, $".env.{environment.ToLower()}");
-        var defaultEnvFile = Path.Combine(envFolder, $".env.{nameof(EnvironmentType.Local).ToLower()}");
         
-        DotNetEnv.Env.Load(File.Exists(envFile) ? envFile : defaultEnvFile);
-
-        builder.Configuration.AddEnvironmentVariables();
-
-        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-        builder.Services.AddControllers();
-        builder.Services.AddLogging();
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddRelationalContext();
-        builder.Services.AddJwtTokenConfiguration(builder.Configuration.GetSection("AuthenticationTokenSettings"));
-        builder.Services.AddModelValidators();
-        builder.Services.AddRelationalRepositories();
-        builder.Services.AddServices();
-
-        builder.Services.Configure<ApiBehaviorOptions>(options =>
-        {
-            options.InvalidModelStateResponseFactory = context =>
-            {
-                var errors = context.ModelState
-                    .Where(e => e.Value?.Errors.Count > 0)
-                    .Select(e => $"Parameter: {e.Key} | Error: {e.Value?.Errors.First().ErrorMessage}").ToArray();
-
-                DataOutput<string> output = new(string.Empty, errors, false);
-
-                return new BadRequestObjectResult(output);
-            };
-        });
-        
-        builder.Services.AddAuthentication("Jwt").AddJwtBearer("Jwt");
-
-        builder.Services.AddAuthorization();
+        var startup = new Startup(builder.Configuration, builder.Environment);
+        startup.ConfigureServices(builder.Services);
 
         var app = builder.Build();
-
-        app.Services.CreateAsyncScope().ServiceProvider
-            .GetRequiredService<RelationalDbInitializer>()
-            .InitializeAsync()
-            .GetAwaiter()
-            .GetResult();
-
-        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Local"))
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseCors(x => x
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-
-        app.UseMiddleware<ExceptionMiddleware>();
-        app.UseMiddleware<JwtMiddleware>();
-        app.UseHttpsRedirection();
-        app.UseAuthentication();
-        app.UseAuthorization();
-        app.MapControllers();
+        startup.Configure(app, app.Services);
 
         app.Run();
     }
---
+++ src/Application/ArturRios.UserManagement.WebApi/Startup.cs
@@ -0,0 +1,88 @@
+﻿using ArturRios.Common.Environment;
+using ArturRios.Common.Output;
+using ArturRios.Common.WebApi;
+using ArturRios.Common.WebApi.Security.Middleware;
+using ArturRios.UserManagement.Data.Configuration;
+using ArturRios.UserManagement.WebApi.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArturRios.UserManagement.WebApi
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            var envFolder = Path.Combine(basePath, "Environments");
+            var envFile = Path.Combine(envFolder, $".env.{environment.EnvironmentName.ToLower()}");
+            var defaultEnvFile = Path.Combine(envFolder, $".env.{nameof(EnvironmentType.Local).ToLower()}");
+            
+            DotNetEnv.Env.Load(File.Exists(envFile) ? envFile : defaultEnvFile);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddControllers();
+            services.AddLogging();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddRelationalContext();
+            services.AddJwtTokenConfiguration(Configuration.GetSection("AuthenticationTokenSettings"));
+            services.AddModelValidators();
+            services.AddRelationalRepositories();
+            services.AddServices();
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState
+                        .Where(e => e.Value?.Errors.Count > 0)
+                        .Select(e => $"Parameter: {e.Key} | Error: {e.Value?.Errors.First().ErrorMessage}").ToArray();
+                    DataOutput<string> output = new(string.Empty, errors, false);
+                    
+                    return new BadRequestObjectResult(output);
+                };
+            });
+            services.AddAuthentication("Jwt").AddJwtBearer("Jwt");
+            services.AddAuthorization();
+        }
+
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider)
+        {
+            serviceProvider.CreateScope().ServiceProvider
+                .GetRequiredService<RelationalDbInitializer>()
+                .InitializeAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+            
+            if (env.IsDevelopment() || env.IsEnvironment("Local"))
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseMiddleware<JwtMiddleware>();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
