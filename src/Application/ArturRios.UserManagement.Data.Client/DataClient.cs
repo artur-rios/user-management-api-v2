b99fcf56a43006609154fea513187ce968f6cf3d
Artur Rios <arturdev@duck.com>
2025-09-09 00:49:18 -0300

    feat: use base data client

--- src/Application/ArturRios.UserManagement.Data.Client/DataClient.cs
+++ src/Application/ArturRios.UserManagement.Data.Client/DataClient.cs
@@ -1,26 +1,21 @@
 ﻿using ArturRios.Common.Configuration.Enums;
-using ArturRios.Common.Configuration.Loaders;
+using ArturRios.Common.Data;
 using ArturRios.UserManagement.Data.Client.Configuration;
 using ArturRios.UserManagement.Data.Client.Repositories;
 
 namespace ArturRios.UserManagement.Data.Client;
 
-public class DataClient
+public class DataClient : BaseDataClient
 {
     public UserRepository UserRepository { get; }
-
-    public DataClient(EnvironmentType environment)
+    
+    public DataClient(EnvironmentType environment) : base(environment)
     {
-        var configLoader = new ConfigurationLoader(environment.ToString());
-        configLoader.LoadEnvironment();
-        
         UserRepository = new UserRepository(new ClientDbContextFactory());
     }
 
-    public DataClient(string connectionString)
+    public DataClient(string connectionString) : base(connectionString)
     {
-        Environment.SetEnvironmentVariable("DATABASE_CONNECTION_STRING", connectionString);
-        
         UserRepository = new UserRepository(new ClientDbContextFactory());
     }
 }
